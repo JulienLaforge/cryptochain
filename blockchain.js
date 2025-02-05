2725e2ec56f9c7ec69eb47791bb0a0ff63036f34
julien <julien.a.laforge@gmail.com>
2025-02-05 23:33:32 +0100

    nonce and difficulty code

--- blockchain.js
+++ blockchain.js
@@ -34,9 +34,9 @@ class Blockchain {
         if(JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) return false;
         
         for (let i = 1; i < chain.length; i++) {
-            const { timestamp, lastHash, hash, data } = chain[i];
+            const { timestamp, lastHash, hash, nonce, difficulty, data } = chain[i];
             const actualLastHash = chain[i - 1].hash;
-            const validatedHash = cryptoHash(timestamp, lastHash, data);
+            const validatedHash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
 
             if (lastHash !== actualLastHash) return false;
             if (hash != validatedHash) return false;
