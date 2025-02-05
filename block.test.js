8369def67b4ed15f6cd0a1334b397a501e2e6d5b
julien <julien.a.laforge@gmail.com>
2025-02-05 23:32:39 +0100

    nonce and difficulty tests

--- block.test.js
+++ block.test.js
@@ -7,11 +7,15 @@ describe('Block', () => {
     const lastHash = 'foo-hash';
     const hash = 'foo-bar';
     const data = ['blockchain', 'data'];
+    const nonce = 1;
+    const difficulty = 1;
     const block = new Block({
         timestamp,
         lastHash,
         hash,
-        data
+        data,
+        nonce,
+        difficulty
     });
 
     it('has a timestamp property', () => {
@@ -28,6 +32,14 @@ describe('Block', () => {
 
     it('has a data property', () => {
         expect(block.data).toEqual(data);
+    });
+
+    it('has a nonce property', () => {
+        expect(block.nonce).toEqual(nonce);
+    });
+
+    it('has a difficulty property', () => {
+        expect(block.difficulty).toEqual(difficulty);
     });
 
     describe('genesis()', () => {
@@ -64,7 +76,21 @@ describe('Block', () => {
         });
 
         it('creates a SHA-256 `hash` based on the proper inputs', () => {
-            expect(minedBlock.hash).toEqual(cryptoHash(minedBlock.timestamp, lastBlock.hash, data));
+            expect(minedBlock.hash)
+                .toEqual(
+                    cryptoHash(
+                        minedBlock.timestamp,
+                        minedBlock.nonce,
+                        minedBlock.difficulty,
+                        lastBlock.hash,
+                        data
+                    )
+                );
+        });
+
+        it('sets a `hash` that matches the difficulty criteria', () => {
+            expect(minedBlock.hash.substring(0, minedBlock.difficulty))
+                .toEqual('0'.repeat(minedBlock.difficulty));
         });
     });
 });
